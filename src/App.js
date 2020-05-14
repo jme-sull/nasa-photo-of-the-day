a559d54fadd6abc0c8bca990a349993d2730e62a
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-05-14 05:46:04 -0400

    inital file structure complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './constants'
+import ImageContainer from './components/image-container.jsx'
+
+
+
 
 function App() {
   return (
+
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>picture goes here</h1>
+      <ImageContainer />
     </div>
   );
 }
---
+++ src/components/image-container.jsx
@@ -0,0 +1,11 @@
+
+
+export default function ImageContainer(props) {
+
+    const { imageSRC } = props
+    
+    
+    
+    return null
+     
+}
